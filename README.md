675780df3677c54b9053c70cf7fa76f66be81028
lichong <578267200@qq.com>
2020-04-12 23:21:29 +0800

    添加docker和docker-compose安装脚本

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # myScript
+
+存放一些linux自动化安装脚本
+
+现已有：jdk1.8,maven,gradle
+后续计划：docker，elk
---
+++ auto_install_docker_and_compose/install_docker_and_compose.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#todo 还未测试
+
+this_os=$(cat /proc/version |grep Debian |wc -l)
+
+if [ $this_os -eq 0 ]
+then
+    echo "暂时只支持Debian..."
+    exit 1
+fi
+
+#下载安装包
+wget https://raw.githubusercontent.com/lichong5258623/mydocker/master/tools/docker-ce_17.12.0-ce-0-debian_amd64.deb
+wget https://raw.githubusercontent.com/lichong5258623/mydocker/master/tools/libltdl7_2.4.6-0.1_amd64.deb
+wget https://raw.githubusercontent.com/lichong5258623/mydocker/master/tools/libapparmor1_2.13.2-9_amd64.deb
+
+#执行安装docker和依赖
+dpkg -i libltdl7_2.4.6-0.1_amd64.deb
+dpkg -i libapparmor1_2.13.2-9_amd64.deb
+dpkg -i docker-ce_17.12.0-ce-0-debian_amd64.deb
+
+echo"docker安装完毕，开始安装docker-compose"
+#下载dockercompose
+
+if [ !-f docker-compose ];then
+    curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o docker-compose
+fi
+mv docker-compose /usr/local/
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+echo "安装完毕"
